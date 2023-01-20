2ee90df4ae61bd51f08b95b244de29650101c71c
CHRISO1503 <christopher_oakes@hotmail.com>
2023-01-20 07:42:55 +0000

    Set proxy secure type to false

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: { proxy: { "/api": " https://flashcards-online-backend.onrender.com" } },
+    server: {
+        proxy: {
+            "/api": {
+                target: "https://flashcards-online-backend.onrender.com",
+                secure: false,
+            },
+        },
+    },
 });
