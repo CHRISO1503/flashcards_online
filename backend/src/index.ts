e5da94d7f423b7e7a821951684e858cc458e26a5
CHRISO1503 <christopher_oakes@hotmail.com>
2023-01-20 07:10:58 +0000

    Added error logging in runMigraions

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,10 @@ const portNumber = 5000;
 
 AppDataSource.initialize()
     .then(async (connection) => {
-        await connection.runMigrations();
+        await connection
+            .runMigrations()
+            .then(() => console.log("migrationsRun"))
+            .catch((err) => console.log(err));
         app.listen(portNumber, () =>
             console.log(`server listening on ${portNumber}`)
         );
