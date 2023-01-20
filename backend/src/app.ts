fe7903e61ddeb90cc6b5927eab263a6a0123e83c
CHRISO1503 <christopher_oakes@hotmail.com>
2023-01-20 10:32:07 +0000

    Enable cors requests to backend api

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import * as express from "express";
 import router from "./routes";
 
+const cors = require("cors");
 const app = express();
+
+app.use(cors);
 app.use(express.json());
 app.use(router);
 
 export default app;
-
