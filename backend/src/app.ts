f1f23b41f022dc734cddde86ebe14209ab98d955
CHRISO1503 <christopher_oakes@hotmail.com>
2023-01-20 11:07:02 +0000

    Cors bugfix .2

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,11 @@
 import * as express from "express";
 import * as cors from "cors";
 import router from "./routes";
+import { Request } from "express";
 
 const app = express();
 
-app.use(cors);
+app.use(cors<Request>());
 app.use(express.json());
 app.use(router);
 
