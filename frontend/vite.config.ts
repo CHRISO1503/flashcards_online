3b806acf90383db3ae727e9b98a7a6fe219c7f00
CHRISO1503 <christopher_oakes@hotmail.com>
2023-01-20 07:49:46 +0000

    Removed unsecure tag from proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,12 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        proxy: {
-            "/api": {
-                target: "https://flashcards-online-backend.onrender.com",
-                secure: false,
-            },
-        },
-    },
+    server: { proxy: { "/api": "https://flashcards-online-backend.onrender.com" } },
 });
