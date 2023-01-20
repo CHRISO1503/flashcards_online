9fdc5b9a05d16630d9c9e05b77166a70c1f71fad
CHRISO1503 <christopher_oakes@hotmail.com>
2023-01-20 06:55:00 +0000

    Creating render database compatibility

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 import { Deck } from "./entity/Deck";
 import { User } from "./entity/User";
 import { Card } from "./entity/Card";
@@ -9,22 +9,49 @@ import { migrations1672949798582 } from "../database/migrations/1672949798582-mi
 import { migrations1673026279026 } from "../database/migrations/1673026279026-migrations";
 import { migrations1673109817825 } from "../database/migrations/1673109817825-migrations";
 
-export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5433,
-    username: "postgres",
-    password: "LNHmlfI3zd7nGRsyRRbY",
-    database: "flashcards_online",
-    synchronize: false,
-    logging: false,
-    entities: [User, Deck, Card],
-    migrations: [
-        migrationName1671806248117,
-        addPasswords1672332371596,
-        migrations1672949798582,
-        migrations1673026279026,
-        migrations1673109817825,
-    ],
-    subscribers: [],
-});
+const DEPLOYED = true;
+let dataSourceSettings: DataSourceOptions;
+
+if (DEPLOYED) {
+    dataSourceSettings = {
+        type: "postgres",
+        host: "dpg-cf53g8ta4992g5g02lig-a",
+        port: 5432,
+        username: "flashcards_online_user",
+        password: "8wu6vB4PXB2fBlfWIBfDou0VkdupgD74",
+        database: "flashcards_online",
+        synchronize: false,
+        logging: false,
+        entities: [User, Deck, Card],
+        migrations: [
+            migrationName1671806248117,
+            addPasswords1672332371596,
+            migrations1672949798582,
+            migrations1673026279026,
+            migrations1673109817825,
+        ],
+        subscribers: [],
+    };
+} else {
+    dataSourceSettings = {
+        type: "postgres",
+        host: "localhost",
+        port: 5433,
+        username: "postgres",
+        password: "LNHmlfI3zd7nGRsyRRbY",
+        database: "flashcards_online",
+        synchronize: false,
+        logging: false,
+        entities: [User, Deck, Card],
+        migrations: [
+            migrationName1671806248117,
+            addPasswords1672332371596,
+            migrations1672949798582,
+            migrations1673026279026,
+            migrations1673109817825,
+        ],
+        subscribers: [],
+    };
+}
+
+export const AppDataSource = new DataSource(dataSourceSettings);
