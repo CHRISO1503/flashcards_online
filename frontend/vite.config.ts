5a3596c13967d4dc73798780f674a52956f221aa
CHRISO1503 <christopher_oakes@hotmail.com>
2023-01-20 08:25:32 +0000

    Added deployment proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: { proxy: { "/api": "https://flashcards-online-backend.onrender.com" } },
+    server: {
+        proxy: {
+            "/api": "https://flashcards-online-backend.onrender.com",
+            "https://flashcards-online-frontend.onrender.com":
+                "https://flashcards-online-backend.onrender.com",
+        },
+    },
 });
