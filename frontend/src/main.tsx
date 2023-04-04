e1df0c2b90bae508507521ed87dbd8461da25934
CHRISO1503 <christopher_oakes@hotmail.com>
2023-04-04 11:50:15 +0100

    Changed broswerRouter to hashRouter

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Home from "./routes/home";
 import CreateDeck from "./routes/createDeck";
 import OpenDeck from "./routes/openDeck";
 import ManageDecks from "./routes/manageDecks";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     { path: "/", element: <Home /> },
     { path: "/create-deck", element: <CreateDeck /> },
     { path: "/open-deck", element: <OpenDeck /> },
